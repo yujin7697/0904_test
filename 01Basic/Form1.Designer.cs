241641a44b7b7b4b2d0a1b11ba227e5d99f839e6
jangyujin <juj7697@gmail.com>
2023-09-01 13:14:47 +0900

    test

--- 01Basic/Form1.Designer.cs
+++ 01Basic/Form1.Designer.cs
@@ -30,12 +30,24 @@
         {
             this.PortNumber = new System.Windows.Forms.ComboBox();
             this.conn_btn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.textArea = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.TEMP_BOX = new System.Windows.Forms.TextBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.sun_txt = new System.Windows.Forms.TextBox();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.DIS_TXT = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
             this.SuspendLayout();
             // 
             // PortNumber
@@ -46,98 +58,174 @@
             this.PortNumber.IntegralHeight = false;
             this.PortNumber.Items.AddRange(new object[] {
             "COM1",
-            "COM2"});
-            this.PortNumber.Location = new System.Drawing.Point(47, 47);
+            "COM2",
+            "COM7",
+            "COM8"});
+            this.PortNumber.Location = new System.Drawing.Point(15, 20);
             this.PortNumber.Name = "PortNumber";
-            this.PortNumber.Size = new System.Drawing.Size(121, 20);
+            this.PortNumber.Size = new System.Drawing.Size(82, 20);
             this.PortNumber.TabIndex = 0;
             this.PortNumber.SelectedIndexChanged += new System.EventHandler(this.PortNumber_SelectedIndexChanged);
             // 
             // conn_btn
             // 
-            this.conn_btn.Location = new System.Drawing.Point(208, 47);
+            this.conn_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.conn_btn.Location = new System.Drawing.Point(124, 47);
             this.conn_btn.Name = "conn_btn";
             this.conn_btn.Size = new System.Drawing.Size(75, 23);
             this.conn_btn.TabIndex = 1;
             this.conn_btn.Text = "연결";
-            this.conn_btn.UseVisualStyleBackColor = true;
+            this.conn_btn.UseVisualStyleBackColor = false;
             this.conn_btn.Click += new System.EventHandler(this.conn_btn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.PortNumber);
+            this.groupBox1.Location = new System.Drawing.Point(21, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(190, 50);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(21, 100);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(190, 102);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "LED 제어";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(184, 105);
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button2.Location = new System.Drawing.Point(103, 41);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(99, 49);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "LED_01 OFF";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "OFF";
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(47, 105);
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button1.Location = new System.Drawing.Point(15, 41);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(99, 49);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "LED_01 ON";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "ON";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button3
+            // textArea
             // 
-            this.button3.Location = new System.Drawing.Point(47, 171);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(99, 49);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "LED_02 ON";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.textArea.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.textArea.Location = new System.Drawing.Point(6, 21);
+            this.textArea.Multiline = true;
+            this.textArea.Name = "textArea";
+            this.textArea.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textArea.Size = new System.Drawing.Size(190, 212);
+            this.textArea.TabIndex = 1;
             // 
-            // button4
+            // groupBox3
             // 
-            this.button4.Location = new System.Drawing.Point(184, 171);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(99, 49);
-            this.button4.TabIndex = 8;
-            this.button4.Text = "LED_02 OFF ";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.groupBox3.Controls.Add(this.textArea);
+            this.groupBox3.Location = new System.Drawing.Point(259, 28);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(202, 245);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "로그 확인";
             // 
-            // label1
+            // groupBox4
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(45, 250);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(66, 12);
-            this.label1.TabIndex = 9;
-            this.label1.Text = "Message :";
+            this.groupBox4.Controls.Add(this.TEMP_BOX);
+            this.groupBox4.Location = new System.Drawing.Point(21, 218);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(190, 55);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "온도 센서";
             // 
-            // label2
+            // TEMP_BOX
             // 
-            this.label2.AutoSize = true;
-            this.label2.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label2.Location = new System.Drawing.Point(45, 278);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 12);
-            this.label2.TabIndex = 10;
+            this.TEMP_BOX.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.TEMP_BOX.Enabled = false;
+            this.TEMP_BOX.Location = new System.Drawing.Point(6, 20);
+            this.TEMP_BOX.Multiline = true;
+            this.TEMP_BOX.Name = "TEMP_BOX";
+            this.TEMP_BOX.Size = new System.Drawing.Size(178, 23);
+            this.TEMP_BOX.TabIndex = 0;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.sun_txt);
+            this.groupBox5.Location = new System.Drawing.Point(21, 279);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(190, 58);
+            this.groupBox5.TabIndex = 6;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "조도 센서";
+            // 
+            // sun_txt
+            // 
+            this.sun_txt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.sun_txt.Enabled = false;
+            this.sun_txt.Location = new System.Drawing.Point(6, 20);
+            this.sun_txt.Multiline = true;
+            this.sun_txt.Name = "sun_txt";
+            this.sun_txt.Size = new System.Drawing.Size(178, 23);
+            this.sun_txt.TabIndex = 0;
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.DIS_TXT);
+            this.groupBox6.Location = new System.Drawing.Point(259, 282);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(202, 55);
+            this.groupBox6.TabIndex = 1;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "초음파 센서";
+            // 
+            // DIS_TXT
+            // 
+            this.DIS_TXT.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.DIS_TXT.Enabled = false;
+            this.DIS_TXT.Location = new System.Drawing.Point(6, 17);
+            this.DIS_TXT.Multiline = true;
+            this.DIS_TXT.Name = "DIS_TXT";
+            this.DIS_TXT.Size = new System.Drawing.Size(190, 26);
+            this.DIS_TXT.TabIndex = 0;
             // 
             // winform
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(346, 328);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.button2);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(485, 361);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.conn_btn);
-            this.Controls.Add(this.PortNumber);
+            this.Controls.Add(this.groupBox1);
             this.Name = "winform";
             this.Text = "WINFORMTEST";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -145,12 +233,18 @@
 
         private System.Windows.Forms.ComboBox PortNumber;
         private System.Windows.Forms.Button conn_btn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textArea;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox TEMP_BOX;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.TextBox sun_txt;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.TextBox DIS_TXT;
     }
 }
 
--- 01Basic/Form1.cs
+++ 01Basic/Form1.cs
@@ -13,7 +13,9 @@ namespace _01Basic
 {
     public partial class winform : Form
     {
+
         private SerialPort serialPort = new SerialPort();
+
         public winform()
         {
             InitializeComponent();
@@ -21,55 +23,88 @@ namespace _01Basic
 
         private void PortNumber_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            //Console.WriteLine("HELLO WORLD");
+            //Console.WriteLine("sender : " + sender);
+            //Console.WriteLine("EventArgs : " + e);
+            ComboBox cb = (ComboBox)sender;
+            Console.Write("Selected Idx : " + cb.SelectedIndex + "  ");
+            Console.WriteLine("Selected Value : " + cb.Items[cb.SelectedIndex]);
 
         }
 
-        private void SerialPort_DataRecv(object sender, SerialDataReceivedEventArgs e)
-        {
-            String recvData = this.serialPort.ReadExisting();
-            Console.WriteLine(recvData);
-        }
         private void conn_btn_Click(object sender, EventArgs e)
         {
-           
-            
+            Console.WriteLine("Conn_Btn Click : " + this.PortNumber.Items[this.PortNumber.SelectedIndex].ToString()); ;
+            try
+            {
                 this.serialPort.PortName = this.PortNumber.Items[this.PortNumber.SelectedIndex].ToString();
                 this.serialPort.BaudRate = 9600;
                 this.serialPort.DataBits = 8;
                 this.serialPort.StopBits = System.IO.Ports.StopBits.One;
                 this.serialPort.Parity = System.IO.Ports.Parity.None;
-                Console.WriteLine(PortNumber.Items[this.PortNumber.SelectedIndex]+" CONN");
-                this.serialPort.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataRecv);
-            label2.Text = PortNumber.Items[this.PortNumber.SelectedIndex] + " CONN";
+                this.serialPort.Open();
+                Console.WriteLine("CONNECTION SUCCESS");
+                this.textArea.AppendText("Connected...\r\n");
+                this.serialPort.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataReceived);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                this.serialPort.Close();
+                this.textArea.AppendText("Fail..." + ex + "\r\n");
+            }
 
+
+        }
+        private void SerialPort_DataReceived(object sender, EventArgs e)
+        {
+            String recvData = this.serialPort.ReadLine();
+            Console.Write(recvData);
+            //Invoke(), 스레드 생성 코드
+            //Invoke(new Action(() => { /*처리로직*/ }));
+
+            //LED 점등 유무 확인 스레드
+            if (recvData.StartsWith("LED_")) {
+                Invoke(new Action(() => { Console.WriteLine(recvData); this.textArea.AppendText(recvData + "\r\n"); }));
+            }
+
+            //온도 센서 확인 스레드
+            if (recvData.StartsWith("TEMP : ")) {
+                Invoke(new Action(() => { this.TEMP_BOX.Text = ""; this.TEMP_BOX.Text = recvData.Replace("TEMP : ", ""); }));
+            }
+
+            //조도 센서 확인 스레드
+            if (recvData.StartsWith("SUN : "))
+            {
+                Invoke(new Action(() => { this.sun_txt.Text = ""; this.sun_txt.Text = recvData.Replace("SUN : ", ""); }));
+            }
+            //초음파 센서 확인 스레드
+            if (recvData.StartsWith("DIS : "))
+            {
+                Invoke(new Action(() => { this.DIS_TXT.Text = ""; this.DIS_TXT.Text = recvData.Replace("DIS : ", ""); }));
+            }
+        }
+
+
+        private void textArea_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void winform_Load(object sender, EventArgs e)
+        {
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Console.WriteLine("LED_01 ON CLICKED");
-            label2.Text = "LED_01 ON SUCCESS";
+            serialPort.Write("1");
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Console.WriteLine("LED_01 OFF CLICKED");
-            label2.Text = "LED_01 OFF SUCCESS";
+            serialPort.Write("0");
         }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            Console.WriteLine("LED_02 ON CLICKED");
-            label2.Text = "LED_02 ON SUCCESS";
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            Console.WriteLine("LED_02 OFF CLICKED");
-            label2.Text = "LED_02 OFF SUCCESS";
-        }
-
-        
     }
 }
